f97eabbf7168edcdb64fabab678621d8f61230a4
iury_resend <iuryresende628@gmail.com>
2021-01-15 17:22:33 -0300

    First script

---
+++ layer-test.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+X = [
+	[1.0, 2.0, 3.0, 2.5],
+	[2.0, 5.0, -1.0, 2.0],
+	[-1.5, 2.7, 3.3, -0.8]
+]
+
+
+class LayerDense:
+	def __init__(self, n_inputs, n_neurons):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+		self.biases = np.zeros((1, n_neurons))
+		self.output = []
+
+	def forward(self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = LayerDense(4, 5)
+layer2 = LayerDense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
